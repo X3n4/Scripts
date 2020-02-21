995791b8b049ebba4d6de7dbff701bad0ee331c6
X3n4 <57630277+X3n4@users.noreply.github.com>
2020-02-21 08:54:30 -0500

    Update PaSuReTo.sh

--- PaSuReTo.sh
+++ PaSuReTo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Start script, accompanied by domain to scan. 
+
 wget https://spyse.com/search/subdomain?q=$1
-mkdir Documents/$1
-cat subdomain\?q\=$1 | grep data-domain | cut -d "\"" -f4 > Documents/$1/subdomeinen.txt
+mkdir ~/$1
+cat subdomain\?q\=$1 | grep data-domain | cut -d "\"" -f4 > ~/$1/subdomains.txt
  
